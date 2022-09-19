b47198783fc0039c96949aabcae3ae6acf0fd639
Dy-6114 <1980506537@qq.com>
2022-09-19 14:41:29 +0800

    排好序好查找第k个元素

--- 8.3/05.cpp
+++ 8.3/05.cpp
@@ -26,7 +26,7 @@ int partition(int a[], int low, int high)
 
 void quicksort(int a[], int low, int high)
 {
-    while (low < high)
+    if (low < high)
     {
         int i = partition(a, low, high);
         quicksort(a, low, i - 1);
@@ -34,50 +34,75 @@ void quicksort(int a[], int low, int high)
     }
 }
 
-// void heapadjust(int a[], int k, int len){
-//     a[0] = a[k];
-//     for (int i = 2*k; i < len; i = 2*i)
-//     {
-//         if(a[i+1] < a[i] && i<len) i++; //i<len保证该结点有右孩子
-//         if(a[0]<=a[i]) break; //构建小根堆
-//         else{
-//             a[0] = a[i];
-//             k = i;
-//         }
-//     }
-//     a[k] = a[0];
+void heapadjust(int a[], int k, int len)
+{
+    a[0] = a[k];
+    for (int i = 2 * k; i <= len; i *= 2)
+    {
+        if (i < len && a[i + 1] > a[i])
+            i++; // i<len保证该结点有右孩子
+        if (a[0] >= a[i])
+            break; //构建大根堆
+        else
+        {
+            a[k] = a[i];
+            k = i;
+        }
+    }
+    a[k] = a[0];
+}
 
-// }
+void buidminheap(int a[], int len)
+{
+    for (int i = len / 2; i > 0; i--) //此处没有等于吗，a[0]用来暂时存放元素
+    {
+        heapadjust(a, i, len);
+    }
+}
 
-// void buidminheap(int a[],int len){
-//     for (int i = len/2; i > 0; i--) //此处没有等于吗，a[0]用来暂时存放元素
-//     {
-//         heapadjust(a,i,len);
-//     }
+void heapsort(int a[], int len)
+{
+    buidminheap(a, len);
+    for (int i = len; i > 1; i--)
+    {
+        int tmp = a[i];
+        a[i] = a[1];
+        a[1] = tmp;
+        heapadjust(a, 1, i - 1);
+    }
+}
 
-// }
-
-// void heapsort(int a[],int len){
-//     buidminheap(a,len);
-//     for (int i = len; i > 1; i--)
-//     {
-//         int tmp = a[i];
-//         a[i] = a[1];
-//         a[1] = tmp;
-//         heapadjust(a,1,i-1);
-//     }
-
-// }
+int kth_elem(int a[], int low, int high, int k)
+{
+    int pivot = a[low];
+    int low_temp = low, high_temp = high;
+    while (low < high)
+    {
+        while (low < high && a[high] >= pivot)
+            --high; //两个while循环判断条件有等号
+        a[low] = a[high];
+        while (low < high && a[low] <= pivot)
+            ++low;
+        a[high] = a[low];
+    }
+    a[low] = pivot;
+    if (low == k)
+        return a[low];
+    else if (low > k)
+        return kth_elem(a, low_temp, low - 1, k);
+    else
+        return kth_elem(a, low + 1, high_temp, k);
+}
 
 int main()
 {
     int a[] = {1, 7, 5, 4, 9, 11, 14, 10, 15, 3};
-    int b[] = {0, 1, 7, 5, 4, 9, 11, 14, 10, 15, 3};
-    int c[] = {1, 7, 5, 4, 9, 11, 14, 10, 15, 3};
+    int b[] = {0, 1, 7, 5, 4, 9, 11, 14, 10, 15, 3}; // a[0]作为哨兵
+    int c[] = {0, 19, 7, 25, 4, 9, 11, 14, 10, 15, 3}; // a[0]不用
     quicksort(a, 0, 9);
-    printf("算法一：第四个元素为%d\n,", a[3]);
-    // heapsort(b,10);
-    // printf("算法二：第四个元素为%d\n,", b[3]);
-    // printf("算法三：第四个元素为%d\n,", c[3]);
+    printf("算法一：第四个元素为%d\n", a[3]);
+    heapsort(b, 10);
+    printf("算法二：第四个元素为%d\n", b[4]);
+    printf("算法三：第四个元素为%d\n", kth_elem(c, 1, 10, 4));
     return 0;
 }
